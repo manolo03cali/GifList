e4c0d2de05ab594ec569fb345e752cd674fd64b6
Manuel <manolo.ing@gmail.com>
2025-09-05 11:45:29 -0500

    cache hook

--- src/gifs/interfaces/giphy.response.ts
+++ src/gifs/interfaces/giphy.response.ts
@@ -100,14 +100,9 @@ export interface Looping {
   mp4: string;
 }
 
-export enum Rating {
-  G = "g",
-  PG = "pg",
-}
+export type Rating = "G" | "PG";
 
-export enum Type {
-  GIF = "gif",
-}
+export type Type = "gif";
 
 export interface User {
   avatar_url: string;
